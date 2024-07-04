85d00c76ad7e19219724f9baa4d7581e20743bff
shahin <sh.rahimi.dev@gmail.com>
2024-07-04 13:22:35 +0330

    add placeholder for CRUD opertation on user and alert

--- storage.go
+++ storage.go
@@ -8,6 +8,19 @@ import (
 )
 
 type Storage interface {
+	GetUser(id string) (*User, error)
+	GetUsers() ([]User, error)
+	GetUserByUserID(userID int64) (*User, error)
+	CreateUser(user User) error
+	UpdateUser(id string, u User) error
+	DeleteUser(id string) error
+
+	GetAlert(id string) (*Alert, error)
+	GetAlerts() ([]Alert, error)
+	GetAlertsByUserID(userID int64) ([]Alert, error)
+	CreateAlert(a Alert) error
+	UpdateAlert(id string, a Alert) error
+	DeleteAlert(id string) error
 }
 
 type SqliteStore struct {
@@ -38,5 +51,43 @@ func (s *SqliteStore) Init() error {
 		log.Println("Cant create alerts table", err)
 		return err
 	}
+	return nil
+}
+
+func (s *SqliteStore) GetUser(id string) (*User, error) {
+	return nil, nil
+}
+func (s *SqliteStore) GetUsers() ([]User, error) {
+	return nil, nil
+}
+func (s *SqliteStore) GetUserByUserID(userID int64) (*User, error) {
+	return nil, nil
+}
+func (s *SqliteStore) CreateUser(user User) error {
+	return nil
+}
+func (s *SqliteStore) UpdateUser(id string, user User) error {
+	return nil
+}
+func (s *SqliteStore) DeleteUser(id string) error {
+	return nil
+}
+
+func (s *SqliteStore) GetAlert(id string) (*Alert, error) {
+	return nil, nil
+}
+func (s *SqliteStore) GetAlerts() ([]Alert, error) {
+	return nil, nil
+}
+func (s *SqliteStore) GetAlertsByUserID(userID int64) ([]Alert, error) {
+	return nil, nil
+}
+func (s *SqliteStore) CreateAlert(alert Alert) error {
+	return nil
+}
+func (s *SqliteStore) UpdateAlert(id string, alert Alert) error {
+	return nil
+}
+func (s *SqliteStore) DeleteAlert(id string) error {
 	return nil
 }
