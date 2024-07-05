d7b1468ff30d997f14f64a84953427874666b71a
shahin <sh.rahimi.dev@gmail.com>
2024-07-05 22:11:20 +0330

    nice functions to handle

--- bot.go
+++ bot.go
@@ -44,84 +44,129 @@ func (b *TelegramBot) Init() {
 		log.Println("Error finding usersIDs for users", err)
 	}
 
-	b.bot.Handle("/echo", b.HandleEcho())
-	b.bot.Handle("/addalert", b.HandleAddAlert())
+	b.bot.Handle("/start", makeHandleFunc(b.HandleLogin))
+	b.bot.Handle("/deleteme", makeHandleFunc(b.HandleDeleteMe))
+
+	b.bot.Handle("/echo", makeHandleFunc(b.HandleButtons))
+	b.bot.Handle("/addalert", makeHandleFunc(b.HandleAddAlert))
+	b.bot.Handle("/viewalerts", makeHandleFunc(b.HandleViewAlerts))
+	b.bot.Handle("/deletealert", makeHandleFunc(b.HandleDeleteAlert))
+	b.bot.Handle("/updatealert", makeHandleFunc(b.HandleUpdateAlert))
+	b.bot.Handle(tele.OnCallback, makeHandleFunc(b.HandleButtons))
 
 	adminsOnly := b.bot.Group()
 	adminsOnly.Use(middleware.Whitelist(adminIDs...))
-	adminsOnly.Handle("/helloadmin", b.HandleHelloAdmin())
-	adminsOnly.Handle("/test", b.HandleTest())
 	usersOnly := b.bot.Group()
 	usersOnly.Use(middleware.Whitelist(userIDs...))
-	usersOnly.Handle("/test", b.HandleTest())
-	// usersOnly.Handle("/addalert", b.HandleAddAlert())
-	usersOnly.Handle("/deletealert", func(c tele.Context) error {
-		args := strings.Fields(c.Text())
-		if len(args) < 3 {
-			return c.Send("Usage: /deletealert <id>")
-		}
-		return nil
-	})
-	usersOnly.Handle("/viewalerts", func(c tele.Context) error {
-		args := strings.Fields(c.Text())
-		if len(args) < 3 {
-			return c.Send("Usage: /viewalerts <id>")
-		}
-		return nil
-	})
 
 	b.bot.Start()
 }
 
-func (b *TelegramBot) HandleEcho() tele.HandlerFunc {
+type HandleFunc func(c tele.Context) error
+type ApiError struct {
+	Error string `json:"error"`
+}
+
+// public handlers
+func (b *TelegramBot) HandleLogin(c tele.Context) error {
+	userID := c.Sender().ID
+	if _, err := b.store.GetUserByUserID(userID); err == nil {
+		return c.Send("You are already registered!")
+	}
+	user, err := NewUser(userID, c.Sender().Username, "default_passs")
+	if err != nil {
+		return c.Send("Failed to create user object.")
+	}
+	if err := b.store.CreateUser(*user); err != nil {
+		return c.Send("Failed to insert user to users.")
+	}
+	return c.Send("You are registered successfully!")
+
+}
+func (b *TelegramBot) HandleDeleteMe(c tele.Context) error {
+	return nil
+}
+
+// handles
+func (b *TelegramBot) HandleEcho(c tele.Context) error {
+	args := strings.Fields(c.Text())
+	if len(args) < 2 {
+		return c.Send("Usage: /echo <somthing>")
+	}
+	somthing := args[1]
+	return c.Send(somthing)
+}
+func (b *TelegramBot) HandleAddAlert(c tele.Context) error {
+	args := strings.Fields(c.Text())
+	if len(args) < 3 {
+		return c.Send("Usage: /addalert <label> <number><unit>")
+	}
+	label := args[1]
+	durationStr := args[2]
+	userID := c.Sender().ID
+	duration, err := ParseDuration(durationStr)
+	if err != nil {
+		return c.Send("Invalid duration format.")
+	}
+	count, err2 := b.store.GetAlertsCountByUserID(userID)
+	if err2 != true {
+		log.Println("Does not able to count the alerts by userID")
+	}
+	if count >= maxAlerts {
+		return c.Send("You can only create up to 5 alerts.")
+	}
+	triggerAt := time.Now().Add(duration)
+	newAlert := NewAlert(userID, label, triggerAt)
+	if err := b.store.CreateAlert(*newAlert); err != nil {
+		return err
+	}
+	go b.ScheduleAlert(userID, label, triggerAt)
+	return c.Send(fmt.Sprintf("Alert created: %s in %s", label, durationStr))
+}
+func (b *TelegramBot) HandleViewAlerts(c tele.Context) error {
+	return nil
+}
+func (b *TelegramBot) HandleDeleteAlert(c tele.Context) error {
+	return nil
+}
+func (b *TelegramBot) HandleUpdateAlert(c tele.Context) error {
+	return nil
+}
+func (b *TelegramBot) HandleButtons(c tele.Context) error {
+	return nil
+}
+func (b *TelegramBot) HandleSnooz() tele.HandlerFunc {
 	return func(c tele.Context) error {
-		args := strings.Fields(c.Text())
-		if len(args) < 2 {
-			return c.Send("Usage: /echo <somthing>")
+		// Handle snooze logic
+		data := c.Callback().Data
+		fmt.Println("hi there", data)
+		alertID := c.Callback().Data
+		// if err != nil {
+		// 	return c.Send("Invalid alert ID.")
+		// }
+
+		alert, err := b.store.GetAlert(alertID)
+		if err != nil {
+			return c.Send("Alert not found.", alertID)
 		}
-		somthing := args[1]
-		return c.Send(somthing)
+
+		duration := alert.TriggerAt.Sub(time.Now()) / snoozeFraction
+		newTriggerAt := time.Now().Add(duration)
+		alert.TriggerAt = newTriggerAt
+		if err := b.store.UpdateAlert(alertID, *alert); err != nil {
+			return c.Send("Failed to snooze alert.", err)
+		}
+		go b.ScheduleAlert(alert.UserID, alert.Lable, newTriggerAt)
+		return c.Send(fmt.Sprintf("Alert snoozed for %v", duration))
 	}
 }
 
-func (b *TelegramBot) HandleHelloAdmin() tele.HandlerFunc {
+func makeHandleFunc(f HandleFunc) tele.HandlerFunc {
 	return func(c tele.Context) error {
-		return c.Send("Hi Admin")
-	}
-}
-func (b *TelegramBot) HandleTest() tele.HandlerFunc {
-	return func(c tele.Context) error {
-		return c.Send("1\n2\n3\n testing")
-	}
-}
-func (b *TelegramBot) HandleAddAlert() tele.HandlerFunc {
-	return func(c tele.Context) error {
-		args := strings.Fields(c.Text())
-		if len(args) < 3 {
-			return c.Send("Usage: /addalert <label> <number><unit>")
-		}
-		label := args[1]
-		durationStr := args[2]
-		userID := c.Sender().ID
-		duration, err := ParseDuration(durationStr)
-		if err != nil {
-			return c.Send("Invalid duration format.")
-		}
-		count, err2 := b.store.GetAlertsCountByUserID(userID)
-		if err2 != true {
-			log.Println("Does not able to count the alerts by userID")
-		}
-		if count >= maxAlerts {
-			return c.Send("You can only create up to 5 alerts.")
-		}
-		triggerAt := time.Now().Add(duration)
-		newAlert := NewAlert(userID, label, triggerAt)
-		if err := b.store.CreateAlert(*newAlert); err != nil {
+		if err := f(c); err != nil {
 			return err
 		}
-		go b.ScheduleAlert(userID, label, triggerAt)
-		return c.Send(fmt.Sprintf("Alert created: %s in %s", label, durationStr))
-
+		return nil
 	}
 }
 
